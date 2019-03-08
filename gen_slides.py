058f434c9a75252a3025d7d9d3b7a1f56bb84fbc
joaomcteixeira <joaomcteixeira@gmail.com>
2019-03-08 12:03:35 +0100

    arparse in genslides

--- gen_slides.py
+++ gen_slides.py
@@ -1,13 +1,46 @@
+"""
+Slides generator.
+"""
 import sys
 import importlib.machinery
+import argparse
 
 from matplotlib import pyplot as plt
 
-for i, slide_path in enumerate(sys.argv[1:], start=1):
+ap = argparse.ArgumentParser(description=__doc__)
+
+ap.add_argument(
+    "slides",
+    help="list of slides PATHs in page order.",
+    type=str,
+    nargs="+",
+    )
+
+ap.add_argument(
+    "-g",
+    "--grid",
+    help="Adds grid to slides",
+    action="store_true",
+    )
+
+ap.add_argument(
+    "-pg",
+    "--page",
+    help="The starting page number. Defaults to 1.",
+    default=1,
+    type=int,
+    )
+
+args = ap.parse_args()
+
+for i, slide_path in enumerate(args.slides, start=args.page):
     
     sld = importlib.machinery.SourceFileLoader("s", slide_path).load_module()
     
-    fig, ax = sld.slide(i)
+    fig, ax = sld.slide(
+        i,
+        grid=args.grid,
+        )
     
     file_name = f"page_{i:0>4}_{slide_path[:-3]}.pdf"
     
